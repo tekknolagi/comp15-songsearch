184ccd13feac27da49258c57c80003447a986683
Max Bernstein <tekknolagi@gmail.com>
2015-01-30 17:02:17 -0500

    No need for namespaces in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
-/*
- * main.cpp
- *
- */
-
 #include "word_list.h"
-
-using namespace std;
 
 int main (int argc, char *argv[]) {
   WordList db = WordList();
 
   if (argc == 2) // if there is a filename on the command line
     db.read_lyrics(argv[1], false);
-  else { // use a pre-defined filename
+  else // use a pre-defined filename
     db.read_lyrics("rick_db.txt", false);
-  }
 
   db.repl();
 
